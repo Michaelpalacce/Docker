705e8adf55165cfc000b512607fa9231035485dd
Stefan Genov <stefantigro@gmail.com>
2022-01-02 13:41:01 +0200

    [master] Progress of ytdl

--- ytdl-webserver/BUILD.sh
+++ ytdl-webserver/BUILD.sh
@@ -2,12 +2,12 @@
 VERSION=1.0.0
 
 DIR="ytdl-webserver"
-if [ -d "$DIR" ]; then
-  echo "${DIR} missing, cloning repo!"
-  git clone https://github.com/Algram/ytdl-webserver ytdl-webserver
-fi
+#if [ -d "$DIR" ]; then
+#  echo "${DIR} missing, cloning repo!"
+#  git clone https://github.com/Algram/ytdl-webserver ytdl-webserver
+#fi
 
-cd $DIR && git pull && docker buildx build --platform linux/amd64,linux/arm64 \
+cd $DIR && docker buildx build --platform linux/amd64,linux/arm64 \
 -t stefangenov/ytdl-webserver:latest \
 -t stefangenov/ytdl-webserver:"${VERSION}" \
 -f Dockerfile \
